dde25bf99d814268e510a688c4092522413df3e4
Rafael Noronha <rafaelsnoronha@hotmail.com>
2021-05-18 10:19:10 -0300

    Começando a mostrar os dados da empresa no Template

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .models import Empresa
+
 
 def home(request):
-    return render(request, 'home.html')
+    empresa = Empresa.objects.get(id=1)
+
+    context = {
+        'empresa': empresa
+    }
+    return render(request, 'home.html', context)
